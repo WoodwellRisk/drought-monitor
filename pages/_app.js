15eef85c5178012c515f596769d600280fd34a88
jakidxav <p6mmc@proton.me>
2025-07-30 13:39:33 -0700

    adding woodwell fonts to site

--- pages/_app.js
+++ pages/_app.js
@@ -2,9 +2,17 @@ import { ThemeUIProvider } from 'theme-ui'
 import '@carbonplan/components/globals.css'
 import '@carbonplan/components/fonts.css'
 import '@carbonplan/maps/mapbox.css'
+import './stylesheet.css'
 import theme from '@carbonplan/theme'
 
 const App = ({ Component, pageProps }) => {
+  theme.fonts = {
+    body: "ginto-normal",
+    faux: "ginto-normal",
+    heading: "ginto-normal",
+    mono: "ginto-normal",
+  }
+  
   return (
     <ThemeUIProvider theme={theme}>
       <Component {...pageProps} />
