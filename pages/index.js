281bc25b4bed255f2669ead60ec12491da650d2c
jakidxav <p6mmc@proton.me>
2025-12-05 08:24:04 -0800

    removing layout component, rewriting and moving metadata component

--- pages/index.js
+++ pages/index.js
@@ -2,7 +2,7 @@ import { useEffect, useState } from 'react'
 import { Box, useColorMode } from 'theme-ui'
 import { useBreakpointIndex } from '@theme-ui/match-media'
 
-import Layout from '../components/view/layout'
+import Meta from './meta'
 import Desktop from '../components/view/desktop'
 import Mobile from '../components/view/mobile'
 
@@ -10,9 +10,6 @@ function Index() {
   const isWide = useBreakpointIndex() > 0
   const [expanded, setExpanded] = useState(false)
   const [colorMode, setColorMode] = useColorMode()
-  const description = 'Woodwell Climate Research Center drought / crop monitor'
-  const title = 'Woodwell Risk drought / crop monitor'
-  const logoURL = 'https://storage.googleapis.com/risk-maps/media/woodwell-risk.png'
   
   useEffect(() => {
     setColorMode('light')
@@ -20,37 +17,14 @@ function Index() {
 
   return (
     <>
+      <Meta />
+
       {isWide && (
-        <Layout
-          description={description}
-          title={title}
-          header={false}
-          dimmer={false}
-          footer={false}
-          metadata={false}
-          guide={'teal'}
-        >
-          <Desktop />
-        </Layout> 
+        <Desktop />
       )}
       {!isWide && (
         <Box sx={{ display: ['initial', 'none', 'none', 'none'], overflow: "hidden",}}>
-          <Layout
-            description={description}
-            title={title}
-            card={logoURL}
-            header={true}
-            dimmer={true}
-            metadata={false}
-            footer={false}
-            guide={'teal'}
-            settings={{
-              value: expanded,
-              onClick: () => setExpanded(!expanded),
-            }}
-          >
-            <Mobile expanded={expanded} />
-          </Layout>
+          <Mobile expanded={expanded} />
         </Box>
       )}
     </>
