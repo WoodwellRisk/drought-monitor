b68100166e3a16c75fff8065f8423e1ab950e5f0
jakidxav <p6mmc@proton.me>
2026-06-24 08:11:27 -0700

    experimenting with consolidating historical and forecast data by concatenating along integration window

--- data_processing/drought_to_zarr_local.py
+++ data_processing/drought_to_zarr_local.py
@@ -92,8 +92,14 @@ def pyramid_to_zarr(ds: xr.Dataset, levels: int, path: str) -> None:
         None
     """
     # https://github.com/zarr-developers/zarr-python/issues/2964
-    dt = pyramid_reproject(ds.drop_encoding(), levels=levels, other_chunks={'time': len(ds.time)})
-    dt.to_zarr(path, consolidated=True, zarr_format=2, mode='w')
+    dt = pyramid_reproject(
+        ds.drop_encoding(),
+        levels=levels,
+        extra_dim='window',
+        other_chunks={'window': len(ds.window), 'time': len(ds.time)},
+    )
+
+    dt.to_zarr(path, zarr_format=2, consolidated=True, mode='w')
 
 
 def drought_pipeline():
@@ -148,8 +154,6 @@ def drought_pipeline():
     print('Opening data.')
     # open historical data for both integration windows
     # parallelize data reads
-    # note: if we tried to use xr.open_mfdataset(...parallel=True),
-    # we would not be able to extract the time from the filename for each NetCDF
     print('    Opening H3 data...')
     ds_list = open_files_in_parallel(h3_files)
     h3 = xr.concat(ds_list, dim='time')
@@ -167,26 +171,42 @@ def drought_pipeline():
     f3 = f3.rename({'L': 'time', '50%': 'perc'})
     f3 = f3[['time', 'y', 'x', 'spatial_ref', '5%', '20%', 'perc', '80%', '95%']]
 
-    print('    Opening H12 data...')
+    print('    Opening F12 data...')
     print()
     f12 = open_forecast_dataset(f12_file)
     f12 = process_dataset(f12)
     f12 = f12.rename({'L': 'time', '50%': 'perc'})
     f12 = f12[['time', 'y', 'x', 'spatial_ref', '5%', '20%', 'perc', '80%', '95%']]
 
+    print('Concatenating historical datasets.')
+    h = xr.concat([h3, h12], data_vars='all', dim='window').assign_coords(
+        {'window': np.array(['3', '12'], dtype='U10')}
+    )
+
+    print('Concatenating forecast datasets.')
+    print()
+    f = xr.concat([f3, f12], data_vars='all', dim='window').assign_coords(
+        {'window': np.array(['3', '12'], dtype='U10')}
+    )
+
     dataset_dict = {
-        'h3': h3,
-        'h12': h12,
-        'f3': f3,
-        'f12': f12,
+        'h': h,
+        'f': f,
     }
 
     print('Saving Zarr stores used for analysis.')
     # save the data as zarr stores that we will use for analysis
-    for dataset in ['h3', 'h12', 'f3', 'f12']:
-        print(f'    Saving {dataset.upper()} data...')
+    for dataset in dataset_dict.keys():
+        if dataset == 'h':
+            save_print_label = 'historical'
+            save_to = f'gs://{BUCKET}/zarr/analysis/wb-{dataset}.zarr'
+        else:
+            save_print_label = 'forecast'
+            save_to = f'gs://{BUCKET}/zarr/analysis/wb-{dataset}-{year_ic}-{month_ic}-01.zarr'
+
+        print(f'    Saving {save_print_label} data...')
         dataset_dict[dataset].to_zarr(
-            f'gs://{BUCKET}/zarr/analysis/{dataset}-{year_ic}-{month_ic}-01.zarr',
+            save_to,
             consolidated=True,
             zarr_format=2,
             mode='w',
@@ -195,34 +215,38 @@ def drought_pipeline():
 
     print('Saving Zarr stores used for visualization.')
     # next, save the data as zarr stores that we will use for visualization
-    # first, we need to save their time coordinates as strings
-    h3['time'] = [pd.to_datetime(value).strftime('%Y-%m-%d') for value in h3.time.values]
-    h12['time'] = [pd.to_datetime(value).strftime('%Y-%m-%d') for value in h12.time.values]
-    f3['time'] = [pd.to_datetime(value).strftime('%Y-%m-%d') for value in f3.time.values]
-    f12['time'] = [pd.to_datetime(value).strftime('%Y-%m-%d') for value in f12.time.values]
+    # first, we need to save their time coordinates as object arrays
+    h['time'] = np.array(
+        [pd.to_datetime(value).strftime('%Y-%m-%d') for value in h.time.values], dtype='U10'
+    )
+    f['time'] = np.array(
+        [pd.to_datetime(value).strftime('%Y-%m-%d') for value in f.time.values], dtype='U10'
+    )
 
     # redeclare dict since data has changed
     dataset_dict = {
-        'h3': h3,
-        'h12': h12,
-        'f3': f3,
-        'f12': f12,
+        'h': h,
+        'f': f,
     }
 
     # next, calculate the number of zoom levels to use for the output Zarr
     pixels_per_tile = 128
-    longitute_length = h3.perc.x.shape[0]
-    max_levels = round(np.sqrt(longitute_length / pixels_per_tile)) + 1
+    longitude_length = h.perc.x.shape[0]
+    # longitude_length = f.perc.x.shape[0]
+    max_levels = round(np.sqrt(longitude_length / pixels_per_tile)) + 1
     levels = max_levels
 
     # create pyramids for each dataset, then save the pyramid to zarr
-    for dataset in ['h3', 'h12', 'f3', 'f12']:
-        print(f'    Saving {dataset.upper()} data...')
-        pyramid_to_zarr(
-            dataset_dict[dataset],
-            levels,
-            f'gs://{BUCKET}/zarr/viz/{dataset}-{year_ic}-{month_ic}-01.zarr',
-        )
+    for dataset in dataset_dict.keys():
+        if dataset == 'h':
+            save_print_label = 'historical'
+            save_to = f'gs://{BUCKET}/zarr/viz/wb-{dataset}.zarr'
+        else:
+            save_print_label = 'forecast'
+            save_to = f'gs://{BUCKET}/zarr/viz/wb-{dataset}-{year_ic}-{month_ic}-01.zarr'
+
+        print(f'    Saving {save_print_label} data...')
+        pyramid_to_zarr(dataset_dict[dataset], levels, save_to)
     print()
 
     print('Done!')
